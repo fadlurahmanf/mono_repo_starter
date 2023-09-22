cac59a050e507367e101cb589499b4570e3f5f0c
fadlurahmanf <tffajari@gmail.com>
2023-09-22 20:32:19 +0700

    test build flutter 2

--- mapp_setup.sh
+++ mapp_setup.sh
@@ -2,4 +2,9 @@ cd forks && cd alice && pwd && ls \
   && flutter clean && flutter pub get \
   && cd ../.. \
   && cd packages && cd core && cd applink && pwd && ls \
+  && flutter clean && flutter pub get \
+  && cd .. && cd camera && pwd && ls \
   && flutter clean && flutter pub get
+  && cd .. && cd config && pwd && ls \
+  && flutter clean && flutter pub get \
+  && flutter pub run build_runner build --delete-conflicting-outputs
